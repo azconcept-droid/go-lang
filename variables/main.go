bf401e4985eb058a4dff8d52df62a567d73d43f9
Yahaya Azeez <azconcept2016@gmail.com>
2026-03-03 16:17:21 +0100

    go variables intro

---
+++ variables/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// strings
+	var nameOne string = "azeez"
+	var nameTwo = "abdulah"
+	var nameThree string
+
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	nameOne = "icecream"
+	nameThree = "banana"
+
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+}
